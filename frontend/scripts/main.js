129432943eb5902983cced434222475a59010b1d
Joseph Park <josephwjp8@gmail.com>
2025-03-08 19:30:24 -0800

    Project(feat): Changed the main contents of the project (Artist portfolio)

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -3,7 +3,7 @@ function toggleMenu() {
     menu.classList.toggle("visible");
 }
 
-// 캐러셀 기능 수정
+// 자동 캐러셀 기능
 let currentIndex = 0;
 const images = document.querySelectorAll(".carousel-image");
 
@@ -15,5 +15,24 @@ function showNextImage() {
 
 document.addEventListener("DOMContentLoaded", () => {
     images[currentIndex].classList.add("active");
-    setInterval(showNextImage, 3000);
+    setInterval(showNextImage, 2000);
 });
+
+function toggleMenu() {
+    const menu = document.getElementById("side-menu");
+    menu.classList.toggle("visible");
+}
+
+// 팝업 열기
+function openPopup(imageSrc) {
+    const popup = document.getElementById("imagePopup");
+    const popupImage = document.getElementById("popupImage");
+    popupImage.src = imageSrc;
+    popup.style.display = "flex";
+}
+
+// 팝업 닫기
+function closePopup() {
+    document.getElementById("imagePopup").style.display = "none";
+}
+
